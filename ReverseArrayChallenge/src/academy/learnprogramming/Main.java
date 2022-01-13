1152ad7a2940788068a9c1c9e2c5d7ae5a6dc7ce
PKoding_World <92498953+Pklee93@users.noreply.github.com>
2022-01-13 00:55:06 -0500

    Add files via upload

---
+++ ReverseArrayChallenge/src/academy/learnprogramming/Main.java
@@ -0,0 +1,39 @@
+/*
+Description
+- Write a method called reverse() with an int array as a parameter
+- The method should not return any value. In other words, the method is allowed to modify the array parameter.
+- In main() test the reverse() method and print the array both reversed and non-reversed
+- To reverse the array, you have to swap the elements, so that the first element is swapped with the last element and so on
+- For example, if the array is {1,2,3,4,5}, then the reversed array is {5,4,3,2,1}.
+Tip
+    - Create a new console project with the name 'ReverseArrayChallenge'
+ */
+
+package academy.learnprogramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        System.out.println("Enter numbers: ");
+        for(int i=0; i<array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        reverse(array);
+    }
+
+    private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+        int temp;
+        for(int i=0; i<(array.length/2); i++) {
+            temp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = temp;
+        }
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
+}
